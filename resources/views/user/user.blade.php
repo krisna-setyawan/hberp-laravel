60a866f5d19a971e2ab991d9cb0e021f456c5509
Krisna Setyawan <krisn4setyawan@gmail.com>
2023-05-31 14:26:03 +0700

    initial commit

---
+++ resources/views/user/user.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.main')
+
+@section('tittle', 'Dashboard')
+
+@section('content')
+
+    <h1>User</h1>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Auth;
+use App\Http\Controllers\Dashboard;
+use App\Http\Controllers\Users\User;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', [Auth::class, 'index']);
+
+
+Route::get('dashboard', [Dashboard::class, 'index'])->name('dashboard');
+Route::get('user', [User::class, 'index'])->name('user');
